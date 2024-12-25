878d850df67e70c96846cb508df566912582c4a6
Sini Gao <sini.gao@xero.com>
2024-12-25 13:26:40 +1300

    add day one solution

---
+++ day-01/day-01-solution/day-01-solution/LeftAndRightIntListModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace day_01_solution
+{
+    public class LeftAndRightIntListModel
+    {
+        public List<int> LeftLine { get; set; }
+        public List<int> RightLine { get; set; }
+    }
+}
